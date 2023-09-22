2551813264f0b5c8168ea46cb6b8a79f4d1609fa
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-09-22 16:01:11 +0200

    docs: create FSJ7 blog

---
+++ nextjs-blog/components/home-page/hero.js
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+import classes from './hero.module.css';
+
+export default function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/site/OS profile.jpg'
+          alt='An image showing Otshepeng'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Otshepeng</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
+}
---
+++ nextjs-blog/components/layout/logo.js
@@ -0,0 +1,5 @@
+import classes from './logo.module.css';
+
+export default function Logo() {
+  return <div className={classes.logo}>Niquáh's Next Blog</div>;
+}
